71968667fbd27c8f87046a9d0c0563d5d741c5be
鐘宇 <zhongyua@yahoo.com>
2017-08-31 14:27:20 +0800

    Basic configuration

--- src/main/groovy/com/zhongyu/config/PrimaryConfig.groovy
+++ src/main/groovy/com/zhongyu/config/PrimaryConfig.groovy
@@ -1,7 +1,23 @@
 package com.zhongyu.config
 
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver
+import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.JpaVendorAdapter
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import org.springframework.transaction.PlatformTransactionManager
+
+import javax.persistence.EntityManagerFactory
+import javax.sql.DataSource
 
 /**
  * Created by ZhongYu on 31/08/2017.
@@ -12,4 +28,50 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
         transactionManagerRef = "transactionManager",
         basePackages = ["com.bean.primary"])
 class PrimaryConfig {
+
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.primary")
+    DataSource dataSource() {
+        final dataSource = DataSourceBuilder.create().build()
+        return dataSource
+    }
+
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            DataSource dataSource,
+            JpaVendorAdapter jpaVendorAdapter,
+            MultiTenantConnectionProvider multiTenantConnectionProviderImpl,
+            CurrentTenantIdentifierResolver currentTenantIdentifierResolverImpl,
+            JpaProperties jpaProperties
+    ) {
+        Map<String, Object> properties = new HashMap<>()
+        properties.putAll(jpaProperties.getHibernateProperties(dataSource))
+        properties.put("hibernate.multiTenancy", 'SCHEMA')
+        properties.put("hibernate.multi_tenant_connection_provider", multiTenantConnectionProviderImpl)
+        properties.put("hibernate.tenant_identifier_resolver", currentTenantIdentifierResolverImpl)
+
+        final entityManagerFactoryBean = builder
+                .dataSource(dataSource)
+                .properties(properties)
+                .packages("com.bean.primary")
+                .persistenceUnit("primaryPersistenceUnit")
+                .build()
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter)
+        return entityManagerFactoryBean
+    }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory)
+    }
+
+    @Bean
+    JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter()
+    }
+
 }
--- src/main/groovy/com/zhongyu/config/SecondaryConfig.groovy
+++ src/main/groovy/com/zhongyu/config/SecondaryConfig.groovy
@@ -1,8 +1,20 @@
 package com.zhongyu.config
 
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.JpaVendorAdapter
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
+
+import javax.persistence.EntityManagerFactory
+import javax.sql.DataSource
 
 /**
  * Created by ZhongYu on 31/08/2017.
@@ -14,4 +26,32 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
         transactionManagerRef = "transactionManagerSecondary",
         basePackages = ["com.bean.secondary"])
 class SecondaryConfig {
+
+    @Bean(name = "dataSourceSecondary")
+    @ConfigurationProperties(prefix = "spring.datasource.secondary")
+    DataSource dataSource() {
+        final dataSource = DataSourceBuilder.create().build()
+        return dataSource
+    }
+
+    @Bean(name = "entityManagerFactorySecondary")
+    LocalContainerEntityManagerFactoryBean entityManagerFactorySecondary(EntityManagerFactoryBuilder builder,
+                                                                         @Qualifier("dataSourceSecondary") DataSource dataSource,
+                                                                         JpaVendorAdapter jpaVendorAdapter) {
+        final entityManagerFactoryBean = builder
+                .dataSource(dataSource)
+                .packages("com.bean.secondary")
+                .persistenceUnit("secondaryPersistenceUnit")
+                .build()
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter)
+        return entityManagerFactoryBean
+    }
+
+    @Bean(name = "transactionManagerSecondary")
+    PlatformTransactionManager transactionManagerSecondary(
+            @Qualifier("entityManagerFactorySecondary") EntityManagerFactory
+                    entityManagerFactorySecondary) {
+        return new JpaTransactionManager(entityManagerFactorySecondary)
+    }
+
 }
